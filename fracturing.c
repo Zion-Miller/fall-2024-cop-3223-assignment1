3b6668b5568c58de8cb84365daa34db1c3176e95
Zion <zi679670@ucf.edu>
2024-09-06 09:55:19 -0400

    assignment1_Zion_Miller.c

---
+++ fracturing.c
@@ -0,0 +1,26 @@
+//***************************************************************
+// fracturing.c
+// Author: Zion Miller
+// Date: 09/04/2024
+// Class: COP 3223, Professor Parra
+// Purpose: write a program with several internal functions 
+// "fracturing" what would be a large main function into several
+// smaller ones
+// Output: dimensions of a circle based off user input
+// //************************************************************
+
+#define PI 3.14159
+
+double calcculateDistance();    //calculate the distance between points
+
+double calculatePerimeter();    //calculate the perimeter
+
+double calculateArea();    //calculate the area
+
+double calculateWidth();    //calculate the width
+
+double calculateHeight();    //calculate the height
+
+double askForUserInput();    //ask for user input
+
+int main(int argc, char **argv);    //main function
